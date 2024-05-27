046980126ef5c182c17c22a09289cf2203ae888e
MaheshTechnicals <Msvarma10001@gmail.com>
2024-05-27 10:11:22 +0000

    Automatic Code GEN in Playwright

--- tests/locateMultipleElm.spec.js
+++ tests/locateMultipleElm.spec.js
@@ -27,3 +27,27 @@ test("Locate Multiple Elements", async ({ page }) => {
 // page.getByAltText()
 // page.getByTitle()
 // page.getByTestId()
+
+
+
+//------------------------------CODE GEN IN PLAYWRIGHT------------------------------
+
+// Playwright Code GEN - It will open browser with playwright tool. With this you can do task in browser and it will gebrate code for you so you can use it directly m
+
+// Use this cammand to start code gen
+
+// npx playwright codegen
+
+// You have more options like you can specify a file name to store all code on that file automatically. Using this cammand :
+
+// npx playwright codegen -O or --output tests/file.spec.js
+
+// if you want to run this test in any browser you can use -b or --browser chromium like this.
+
+// if you want to use Android or IPhone Device you can use it via --device "iphone 13"
+
+// You can also specify viewport for that use --viewport "1080, 720"
+
+
+
+
